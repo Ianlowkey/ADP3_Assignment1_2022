d59adbb5458e0edff29f6b5435b41bcd8a461dac
ianlouw <ianlouw1997@gmail.com>
2022-04-01 09:46:25 +0200

    216250773 commit

--- src/main/java/za/ac/cput/assignment1/identity.java
+++ src/main/java/za/ac/cput/assignment1/identity.java
@@ -1,4 +1,45 @@
 package za.ac.cput.assignment1;
 
+/*
+    Name: Ian Louw
+    Student Number: 216250773
+ */
+
 public class identity {
+
+    private String firstName, lastName;
+    private int age;
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    @Override
+    public String toString() {
+        return "identity{" +
+                "firstName='" + firstName + '\'' +
+                ", lastName='" + lastName + '\'' +
+                ", age=" + age +
+                '}';
+    }
 }
